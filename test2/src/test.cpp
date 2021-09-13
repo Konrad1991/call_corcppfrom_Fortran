70e8bd87fcd2baf645876f1ef8ae90c5923b1b78
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-09-13 08:45:20 +0200

    tested some ideas

--- test2/src/test.cpp
+++ test2/src/test.cpp
@@ -1,6 +1,7 @@
 #include <Rcpp.h>
 
 #include <vector>
+#include <functional>
 
 
 void test(double* a, double* b) {
@@ -11,6 +12,11 @@ void test(double* a, double* b) {
 
   *a = vec_a[0] + vec_b[0];
   *b = vec_a[0] + vec_b[0];
+
+  //std::vector<std::reference_wrapper<double>> vec_a2(a, a + 1);
+  //std::vector<std::reference_wrapper<double>> vec_b2(b, b + 1);
+
+  //std::reference_wrapper<double> rw_a = *a;
 
   //*a = (*a) + (*b);
   //*b = (*a) + (*b);
